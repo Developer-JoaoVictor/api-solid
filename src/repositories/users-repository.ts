5baedaf8e00bcfad6495833975f54dd77d3c1949
Developer-joaoVictor <joaoliveira.contato@hotmail.com>
2025-12-04 20:05:31 -0300

    Resetando o projeto

--- src/repositories/users-repository.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma, User } from '@prisma/client'
-
-export interface UsersRepository {
-  findById(email: string): Promise<User | null>
-  findByEmail(email: string): Promise<User | null>
-  create(data: Prisma.UserCreateInput): Promise<User>
-}
